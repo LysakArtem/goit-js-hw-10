92a53f7978fa8d61c48e04d27c03974e34ce86de
LysakArtem <Perry.dev7@gmail.com>
2020-10-08 22:56:29 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -5,47 +5,39 @@ import './js/template.js'
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
-  };
+};
+const themeSwitchRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
 
-const themeSwitchRef= document.querySelector('#theme-switch-toggle');
-const bodyRef= document.querySelector('body');
-// const check = menuRef.getAttribute('checked');
-
-// localStorage.setItem('settings','light-theme'); 
-var currentTheme='null';
-const value= localStorage.getItem('settings'); 
-console.log(value)
-    if(value){ 
-    currentTheme=value;
-    }
-    if(currentTheme=='light-theme'){
-        renderlightTheme();
-    }
-    else{
-        themeSwitchRef.checked=true;
-        renderDarkTheme();
-
-    }
-themeSwitchRef.addEventListener('change',onChange);
-function onChange(e){
+let currentTheme = 'null';
+const value = localStorage.getItem('settings'); 
+if(value){ 
+    currentTheme = value;
+}
+if(currentTheme == 'light-theme'){
+    renderlightTheme();
+}
+else{
+    themeSwitchRef.checked = true;
+    renderDarkTheme();
+}
+themeSwitchRef.addEventListener('change', onChange);
+const onChange = (e) => {
     if(!e.target.checked) {
-        currentTheme=Theme.LIGHT;
-        renderlightTheme();
+        currentTheme = Theme.LIGHT;
+        renderLightTheme();
     }
     else{
-        currentTheme=Theme.DARK;
+        currentTheme = Theme.DARK;
         renderDarkTheme()
     }
     localStorage.setItem('settings',currentTheme);
 }
-function renderDarkTheme() {
+const renderDarkTheme = () => {
     bodyRef.classList.remove(Theme.LIGHT);
     bodyRef.classList.add(Theme.DARK);
-    // themeSwitchRef.setAttribute('checked','checked')
 }
-
-function renderlightTheme() {
+const renderLightTheme = () => {
     bodyRef.classList.remove(Theme.DARK);
     bodyRef.classList.add(Theme.LIGHT);
-    // themeSwitchRef.removeAttribute('checked','checked');
 }
--- src/js/template.js
+++ src/js/template.js
@@ -1,7 +1,5 @@
 import templates from '../templates/menu-item.hbs'
 import menu from '../menu.json'
-const markup= templates(menu);
-const menuRef= document.querySelector('.js-menu');
-menuRef.insertAdjacentHTML('beforeend',markup);
-
-console.log(menu)
+const markup = templates(menu);
+const menuRef = document.querySelector('.js-menu');
+menuRef.insertAdjacentHTML('beforeend', markup);
